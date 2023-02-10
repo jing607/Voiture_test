3e8b5e66b0212230639cf928e3f1c59d0faf2021
Haojing XU <leslie.hj.xu.design@gmail.com>
2023-02-10 13:05:21 +0100

    modification file css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-
-
 $(document).ready((e)=>{
-    e.preventDefault;
+    let list, i
 
-    
+    list = $("ul");
+
+    $('#charger').on('click',()=>{ //event jquery and callback json
+
+        $.getJSON('js/data.json', (data)=>{
+            for(i = 0; i <data.voitures.length; i++) {
+
+                list.append(`<li>Marque : ${data.voitures[i].marque}</li>`);
+                list.append(`<li>Année : ${data.voitures[i].annee}</li>`);
+                list.append(`<li>Type : ${data.voitures[i].type}</li>`);
+                list.append(`<li>Etat : ${data.voitures[i].etat}</li>`);
+                list.append(`<li>Description : ${data.voitures[i].description}</li>`);
+                list.append(`<li><img src="${data.voitures[i].image}" alt="${data.voitures[i].marque}"></li>`);
+                console.log(JSON.stringify(data.voitures[i].marque))
+
+            }
+        })
+
+    })
+
 
 })
